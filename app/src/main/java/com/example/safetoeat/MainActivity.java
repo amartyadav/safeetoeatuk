5e71ee3ef26312b520bf1f70bcb0139a316f2e97
Amartya Yadav <Amartya.Yadav@fennex.net>
2021-07-01 22:49:26 +0530

    Added Navigation component (not working)

--- app/src/main/java/com/example/safetoeat/MainActivity.java
+++ app/src/main/java/com/example/safetoeat/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.safetoeat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHost;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,14 +22,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.signOutButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                finish();
-                startActivity(new Intent(MainActivity.this, WelcomeAndAuthentication.class));
-                Toast.makeText(getApplicationContext(), "User Signed Out", Toast.LENGTH_SHORT).show();
-            }
-        });
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.bottom_nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+//        findViewById(R.id.signOutButton).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FirebaseAuth.getInstance().signOut();
+//                finish();
+//                startActivity(new Intent(MainActivity.this, WelcomeAndAuthentication.class));
+//                Toast.makeText(getApplicationContext(), "User Signed Out", Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 }
