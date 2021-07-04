1c2aa8c1c5c3f3e9e9f47d6596dd6937626f4514
Amartya Yadav <amartyadav@live.co.uk>
2021-07-04 18:06:34 +0530

    Successfully able to retrieve API data for location from HomeResults. Not yet formatted.

--- app/src/main/java/com/example/safetoeat/HomeResults.java
+++ app/src/main/java/com/example/safetoeat/HomeResults.java
@@ -5,18 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class HomeResults extends AppCompatActivity {
 
@@ -50,11 +61,13 @@ public class HomeResults extends AppCompatActivity {
                     wayLongitude = location.getLongitude();
                     tv_location.setText(String.format(Locale.UK, "%s -- %s", wayLatitude, wayLongitude));
                     Log.d("SafeToEat", wayLatitude + " " + wayLongitude);
+                    getRestaurantInfo();
                 }
             });
         }
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -71,6 +84,7 @@ public class HomeResults extends AppCompatActivity {
                             wayLongitude = location.getLongitude();
                             tv_location.setText(String.format(Locale.UK, "%s -- %s", wayLatitude, wayLongitude));
                             Log.d("SafeToEat", wayLatitude + " " + wayLongitude);
+                            getRestaurantInfo();
                         }
                     });
                 }
@@ -80,6 +94,43 @@ public class HomeResults extends AppCompatActivity {
                 break;
             }
         }
+    }
+
+    // method to make the request to api and retrieve the results
+    private void getRestaurantInfo () {
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        TextView tv_results = findViewById(R.id.tv_results);
+        // below URL is for actual user lcoation. currently using hardcoded Aberdeen's Union Street's lats and longs
+//        String url = "https://api.ratings.food.gov.uk/Establishments?latitude="+wayLatitude+"&longitude="+wayLongitude+"&maxDistanceLimit=5";
+
+        //values for union street
+        // 57.14544702928209, -2.1030610993024093
+        String url = "https://api.ratings.food.gov.uk/Establishments?latitude="+"57.14544702928209"+"&longitude="+"-2.1030610993024093"+"&maxDistanceLimit=5";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        tv_results.setText(response.toString());
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        tv_results.setText(error.getMessage());
+//                        Log.d("SafeToEat", error.getMessage());
+                    }
+                })
+        {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("x-api-version", "2");
+
+                return params;
+            }
+        };
+        queue.add(jsonArrayRequest);
     }
 
 }
